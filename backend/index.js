1c19267cff16c8ff8645533b6b08059bd5abee85
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-08-16 04:11:13 +0530

    Add missing path module import in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 const Redis = require("ioredis");
 const db = require("./models");
